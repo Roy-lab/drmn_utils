cddf484d9d8506a4569cce6b06657c67270e1113
deborah-chasman <deborah.chasman@gmail.com>
2018-04-03 12:23:22 -0500

    Added specialty scripts

---
+++ find_transitioning_genesets_DRMN/find_and_view_transitionGenesets.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+# Runs findTransitionGenesetsDRMN to identify 
+set -u
+if [[ $# != 9 ]]; then
+	echo "USAGE: find_and_view_transitionGenesets.sh drmn_results_dir output_dir_prefix orderfile ogidsfile srcCell k minExpressionValue maxExpressionValue path_to_Heatmap.awk"
+	echo "drmn_results_dir may be a single directory, or a string pattern for cell type-specific directories, containing #CELL# where each cell type specific result is located."
+	echo "We will be appending the geneset size and threshold to the output dir prefix."
+	exit 2
+fi
+
+results=$1	# DRMN results directory
+out=$2
+order=$3	# celltype order (for columns)
+ogids=$4	# ogids file
+srccell=$5	# source celltype for names
+k=$6
+minExp=$7   # minimum expression for color scale
+maxExp=$8 	# maximum expression for color scale
+HMAWK=$9	# path to heatmap.awk
+
+threshold=0.05
+mingeneset=3
+
+echo $results
+if [[ ! -d ${results} ]]; then
+	echo "No directory: $results"
+	exit
+fi
+
+findTransitionGenesets=./findTransitionGenesetsDRMN
+makeHeatmap=./makeTransitionHeatmaps_DRMN.sh
+
+# Run findTransitionGenesets
+out=${out}_min${mingeneset}_${threshold}
+mkdir ${out}
+echo $out
+${findTransitionGenesets} ${results} ${order} ${ogids} ${srccell} $threshold ${out} $mingeneset
+
+# If results exist, plot.
+if [[ -e ${out}/all_genesets.txt ]]; then
+	ngenes=$(cat ${out}/all_clusterassign.txt | wc -l)
+	nclust=$(cat {out}/all_genesets.txt | wc -l)
+	echo "Found $ngenes total genes that change state."
+	echo "Found $nclust genesets with at least $mingeneset genes."
+	${makeHeatmap} ${out} $k $minExp $maxExp $HMAWK
+fi
+
---
+++ find_transitioning_genesets_DRMN/makeTransitionHeatmaps_DRMN.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+# Modified for DRMN to get appropriate colormaps for expression data  
+
+if [ $# -lt 5 ]
+then
+	echo "Too few arguments $#"
+	echo "Usage: ./makeTransitionHeatmaps.sh outputdir numberofclusters minExpr maxExpr heatmap_script_path"
+	exit
+fi
+OUTDIR=$1
+FIGDIR=$OUTDIR/figs
+if [ -d $FIGDIR ]
+then
+	echo "Please remove the figs directory in $OUTDIR"
+	exit
+fi
+mkdir $FIGDIR
+#MAXCID=`expr $2 - 1` # actually, these are getting offset by 1
+MAXCID=$2
+HALFCID=`echo $MAXCID| awk '{printf("%d\n",($1+1)/2)}' `
+
+MINEXP=$3
+MAXEXP=$4
+HALFEXP=$(echo "$MINEXP $MAXEXP" | awk '{printf("%d\n",$1+($2-$1)/2)}')
+
+HEATMAPPER=$5
+
+#First make the birdseye view of the figures
+#export MAXCID
+#export HALFCID # unnecessary to export...
+
+# colormap string
+modmap="-2:(250,250,250);-1:(255,255,255);1:(77,172,38);$HALFCID:(245,245,245);$MAXCID:(208,28,139) 0:(0,0,200);$HALFEXP:(255,255,255);$MAXEXP:(200,0,0)"
+
+
+
+
+cat $OUTDIR/ordered_clusterset_means.txt| $HEATMAPPER -vC="$modmap" -vStrokeC="-" -vStrokeSC="black" -vFontSize=8 -vL="moduleIDs expr"> $FIGDIR/ordered_clusterset_means.svg
+#Now make the figures for each of the clusters of reasonable size
+for FNAME in `ls $OUTDIR/clusterset*.txt`
+do
+	#echo $FNAME
+	FIGNAME=${FNAME/.txt/.svg}
+	FIGNAME=${FIGNAME/$OUTDIR}
+	FIGNAME=$FIGDIR/$FIGNAME 	
+ 	cat $FNAME | $HEATMAPPER -vC="$modmap" -vStrokeC="-" -vStrokeSC="black" -vFontSize=6 -vL="moduleID expr" > $FIGNAME
+done
+
+cat $OUTDIR/all_assign.txt | $HEATMAPPER -vC="$modmap" -vStrokeC="-" -vStrokeSC="black" -vFontSize=6 -vL="moduleID expr" > $FIGDIR/all_assign.svg
